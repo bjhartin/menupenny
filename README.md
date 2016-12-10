2d27d61ad58d399607f57c60ab04c7277aff3dfa
Brian Hartin <brian.hartin@banno.com>
2016-12-10 08:01:38 -0600

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # menupenny
-Hammerspoon extension to allow keyboard search and navigation menus in OSX
+
+Search and navigate any OSX menu without a mouse.
+
+Inspired by nathancahill/Anycomplete.
+
+![Menupenny demo](https://github.com/bjhartin/menupenny/raw/master/demo.gif)
+
+### Features
+
+Use `cmd+esc` (or your custom keybinding) to activate.  Type to find menu items by text or initial letters, e.g. "FNT" or "NT" would find "File/New Tab" in Chrome.
+
+### Installation
+
+Menupenny is an extension for [Hammerspoon](http://hammerspoon.org/).  Append the code from `menupenny.lua` to your `~/.hammerspoon/init.lua` file.
+
+### Known Issues
+
+See issues.
+
+
+
---
+++ menupenny.lua
@@ -0,0 +1,98 @@
+function concatTables(t1, t2)
+   for k,v in pairs(t2) do 
+      table.insert(t1, v)
+   end
+   return t1
+end
+
+function isValidMenuItem(menuItem)
+   -- Currently affected by a bug in AXEnabled: disabled menus will still show.   
+   return menuItem and menuItem.AXTitle and menuItem.AXTitle ~= "" and menuItem.AXEnabled
+end
+
+function computeMenuItemPath(menuItem, parentPath)
+   if parentPath then 
+      return parentPath .. "/" .. menuItem.AXTitle 
+   else 
+      return menuItem.AXTitle
+   end   
+end
+
+function menuItemsToPaths(menu, parentPath)
+  local result = {}  
+
+  -- Ugh.  My lack of Lua knowledge hurts.  Surely this can be much better.
+  for k,v in pairs(menu) do
+    if isValidMenuItem(v) then
+      local fullPath = computeMenuItemPath(v, parentPath)
+      if v.AXChildren then
+        result = concatTables(result, menuItemsToPaths(v.AXChildren[1], fullPath))
+      else
+        result = concatTables(result, {fullPath})
+      end
+    end
+  end
+  return result
+end
+
+function menuItemsToChoices(menuItems)
+  local choices = hs.fnutils.map(menuItems, function(result)
+    return {
+      ["text"] = result
+    }
+  end)
+  return choices  
+end
+
+function isInitialLetters(query)
+  return string.match(query, "^[A-Z]+$") ~= nil
+end
+
+function matchesInitials(menuItem, queryAsInitials)
+   regex, matchCount = string.gsub(queryAsInitials, "[A-Z]", "%1[^A-Z]+")
+   return string.match(menuItem, regex) ~= nil
+end
+
+function matches(menuItem, query)
+   if isInitialLetters(query) then
+     return matchesInitials(menuItem, query)
+   else  
+     i, j = string.find(menuItem:lower(), query:lower())
+     return i ~= nil
+   end
+end
+
+function searchMenuItems(menuItems, query)
+   if query == "" then
+     query = "NotGonnaBeThere"
+   end
+
+   return hs.fnutils.filter(menuItems, function(item)
+     local m = matches(item, query)
+     return m
+   end)
+end
+
+function init()
+  local current = hs.application.frontmostApplication()
+
+  local chooser = hs.chooser.new(function(chosen)
+    current:activate()
+    current:selectMenuItem(hs.fnutils.split(chosen["text"], "/"))
+  end)
+  chooser:bgDark(false)
+  chooser:searchSubText(false)
+  chooser:show()
+
+  local menu = current.getMenuItems(current)  
+  local menuItemPaths = menuItemsToPaths(menu, nil)
+
+  chooser:queryChangedCallback(function(query)
+    chooser:choices(menuItemsToChoices(searchMenuItems(menuItemPaths, query)))
+  end)
+end
+
+
+hs.hotkey.bind({"cmd"}, "escape", function()
+  init()
+end)
